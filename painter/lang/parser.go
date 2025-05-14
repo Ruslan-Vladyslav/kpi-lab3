f31c168943a4a073c57f3fb076d104b4725482cf
Ruslan-Vladyslav <rgritsai95@gmail.com>
2025-05-14 20:19:30 +0300

    Added operations in op.go, implemented parse functions

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -1,21 +1,110 @@
 package lang
 
 import (
+	"bufio"
+	"fmt"
+	"image/color"
 	"io"
+	"strconv"
+	"strings"
 
 	"github.com/roman-mazur/architecture-lab-3/painter"
 )
 
-// Parser уміє прочитати дані з вхідного io.Reader та повернути список операцій представлені вхідним скриптом.
 type Parser struct {
+	State *painter.State
 }
 
 func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanLines)
+
 	var res []painter.Operation
 
-	// TODO: Реалізувати парсинг команд.
-	res = append(res, painter.OperationFunc(painter.WhiteFill))
-	res = append(res, painter.UpdateOp)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
 
-	return res, nil
+		op, err := parseLine(line)
+		if err != nil {
+			return nil, fmt.Errorf("[Error]: parse error on line '%s': %w", line, err)
+		}
+
+		res = append(res, op)
+	}
+
+	return res, scanner.Err()
+}
+
+func parseLine(line string) (painter.Operation, error) {
+	fields := strings.Fields(line)
+	if len(fields) == 0 {
+		return nil, nil
+	}
+
+	switch fields[0] {
+	case "update":
+		return painter.UpdateOp, nil
+	case "white":
+		return painter.FillInColorOp{Color: color.White}, nil
+
+	case "green":
+		return painter.FillInColorOp{Color: color.RGBA{G: 0xff, A: 0xff}}, nil
+
+	case "rect":
+		if len(fields) != 5 {
+			return nil, fmt.Errorf("[Error]: invalid rect args, expected 4 got %d", len(fields)-1)
+		}
+		x0 := parseFloat(fields[1])
+		y0 := parseFloat(fields[2])
+		x1 := parseFloat(fields[3])
+		y1 := parseFloat(fields[4])
+
+		return painter.BgRectOp{
+			X1: x0,
+			Y1: y0,
+			X2: x1,
+			Y2: y1,
+		}, nil
+
+	case "point":
+		if len(fields) != 3 {
+			return nil, fmt.Errorf("[Error]: invalid point args")
+		}
+		x := parseFloat(fields[1])
+		y := parseFloat(fields[2])
+		return painter.FigureOp{
+			X: x,
+			Y: y,
+		}, nil
+
+	case "move":
+		if len(fields) != 3 {
+			return nil, fmt.Errorf("[Error]: invalid move args")
+		}
+		dx := parseInt(fields[1])
+		dy := parseInt(fields[2])
+		return painter.MoveOp{
+			MX: dx,
+			MY: dy,
+		}, nil
+
+	case "reset":
+		return painter.ResetOp{}, nil
+
+	default:
+		return nil, fmt.Errorf("unknown command: %s", fields[0])
+	}
+}
+
+func parseFloat(s string) float64 {
+	f, _ := strconv.ParseFloat(s, 64)
+	return f
+}
+
+func parseInt(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
 }
--- painter/op.go
+++ painter/op.go
@@ -1,6 +1,7 @@
 package painter
 
 import (
+	"image"
 	"image/color"
 
 	"golang.org/x/exp/shiny/screen"
@@ -9,40 +10,92 @@ import (
 // Operation змінює вхідну текстуру.
 type Operation interface {
 	// Do виконує зміну операції, повертаючи true, якщо текстура вважається готовою для відображення.
-	Do(t screen.Texture) (ready bool)
+	Do(t screen.Texture, s *State) (ready bool)
 }
 
 // OperationList групує список операції в одну.
 type OperationList []Operation
 
-func (ol OperationList) Do(t screen.Texture) (ready bool) {
+func (ol OperationList) Do(t screen.Texture, s *State) (ready bool) {
 	for _, o := range ol {
-		ready = o.Do(t) || ready
+		ready = o.Do(t, s) || ready
 	}
 	return
 }
 
-// UpdateOp операція, яка не змінює текстуру, але сигналізує, що текстуру потрібно розглядати як готову.
 var UpdateOp = updateOp{}
 
 type updateOp struct{}
 
-func (op updateOp) Do(t screen.Texture) bool { return true }
+func (op updateOp) Do(t screen.Texture, s *State) bool { return true }
 
-// OperationFunc використовується для перетворення функції оновлення текстури в Operation.
-type OperationFunc func(t screen.Texture)
+type OperationFunc func(t screen.Texture, s *State)
 
-func (f OperationFunc) Do(t screen.Texture) bool {
-	f(t)
+func (f OperationFunc) Do(t screen.Texture, s *State) bool {
+	f(t, s)
 	return false
 }
 
-// WhiteFill зафарбовує тестуру у білий колір. Може бути викоистана як Operation через OperationFunc(WhiteFill).
-func WhiteFill(t screen.Texture) {
-	t.Fill(t.Bounds(), color.White, screen.Src)
+type State struct {
+	BackgroundColor color.Color
+	BgRect          *image.Rectangle
+	Figures         []image.Point
 }
 
-// GreenFill зафарбовує тестуру у зелений колір. Може бути викоистана як Operation через OperationFunc(GreenFill).
-func GreenFill(t screen.Texture) {
-	t.Fill(t.Bounds(), color.RGBA{G: 0xff, A: 0xff}, screen.Src)
+type FillInColorOp struct {
+	Color color.Color
+}
+
+func (op FillInColorOp) Do(t screen.Texture, s *State) bool {
+	s.BackgroundColor = op.Color
+	return false
+}
+
+type BgRectOp struct {
+	X1, Y1, X2, Y2 float64
+}
+
+func (op BgRectOp) Do(t screen.Texture, s *State) bool {
+
+	r := image.Rect(
+		int(op.X1*float64(t.Size().X)),
+		int(op.Y1*float64(t.Size().Y)),
+		int(op.X2*float64(t.Size().X)),
+		int(op.Y2*float64(t.Size().Y)))
+
+	s.BgRect = &r
+	return false
+}
+
+type FigureOp struct {
+	X, Y float64
+}
+
+func (op FigureOp) Do(t screen.Texture, s *State) bool {
+	x := int(op.X * float64(t.Size().X))
+	y := int(op.Y * float64(t.Size().Y))
+
+	s.Figures = append(s.Figures, image.Pt(x, y))
+	return false
+}
+
+type MoveOp struct {
+	MX, MY int
+}
+
+func (op MoveOp) Do(t screen.Texture, s *State) bool {
+	for i := range s.Figures {
+		s.Figures[i].X += op.MX
+		s.Figures[i].Y += op.MY
+	}
+	return false
+}
+
+type ResetOp struct{}
+
+func (op ResetOp) Do(t screen.Texture, s *State) bool {
+	s.BackgroundColor = color.Black
+	s.BgRect = nil
+	s.Figures = nil
+	return false
 }
